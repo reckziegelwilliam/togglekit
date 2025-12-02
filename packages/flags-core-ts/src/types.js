66e622cd71cb0485d1cfb0b26bf37d8bfd06b97c
Liam <reckziegel.william@gmail.com>
2025-12-01 19:39:10 -0800

    fix(lint): Remove generated files and ignore them in ESLint
    - Remove accidentally committed .d.ts and .js files from src/ directories
    - Add .gitignore files to each package to prevent committing generated files
    - Update ESLint configs to ignore any generated .d.ts and .js files
    - Generated files should only exist in dist/ directory, not src/

--- packages/flags-core-ts/src/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Core type definitions for the Togglekit feature flag system
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
